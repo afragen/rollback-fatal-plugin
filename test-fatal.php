a4b71929966baac9390ae6377f987d0f906910f5
Andy Fragen <andy@thefragens.com>
2022-08-02 20:57:44 -0700

    create

---
+++ test-fatal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Auto-update fatal error check test plugin.
+ *
+ * @package AutoUpdateFatalErrorCheck
+ *
+ * Plugin Name:       Auto-update Fatal Error Check Test Plugin
+ * Plugin URI:        https://github.com/afragen/fatal-plugin
+ * Description:       Set local version to 0 and comment code from plugin for testing.
+ * Version:           0.2.0
+ * Author:            WP Core Contributors
+ * License:           MIT
+ * Requires at least: 5.9
+ * Requires PHP:      5.6
+ * GitHub Plugin URI:   https://github.com/afragen/fatal-plugin
+ */
+
+// Will PHP fatal when activated.
+require 'file-not-found.php';
